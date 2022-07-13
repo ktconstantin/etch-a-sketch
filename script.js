f8f0004fbe2b24c37b1e419ce74bbe354e14eddc
Kevin Constantin <kconstantin@protonmail.com>
2022-07-12 18:47:50 -0700

    change color of gridbox on mouseover

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ for (let n = 1; n <= 256; n++) {
   //box.textContent = n;
   container.appendChild(box);
 }
+
+container.childNodes.forEach(box => box.addEventListener('mouseover', onHover));
+
+function onHover(e) {
+  e.target.style.backgroundColor = randomColor();
+}
+
+function randomRGBIndex() {
+  return (Math.floor(Math.random() * 255));
+}
+
+function randomColor() {
+  return `rgb(${randomRGBIndex()}, ${randomRGBIndex()}, ${randomRGBIndex()})`;
+}
